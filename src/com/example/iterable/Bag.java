9ff55bb4649f6696c9751cb9005d755f218655b4
amydomo <ama2135256@maricopa.edu>
2023-10-26 21:53:58 -0700

    final

---
+++ src/com/example/iterable/Bag.java
@@ -0,0 +1,50 @@
+package com.example.iterable;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Bag<T> implements Container<T> {
+
+    private List<T> container;
+
+    public Bag() {
+        this.container = new ArrayList<>();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return container.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return container.size();
+    }
+
+    @Override
+    public void add(T item) {
+        container.add(item);
+
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return container.iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        Container.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return Container.super.spliterator();
+
+    }
+}
+
